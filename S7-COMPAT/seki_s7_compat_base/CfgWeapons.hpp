b269983ffdd624af520e7a415352a5471a6a38d2
Sean <instantiateseankim@gmail.com>
2021-08-03 00:24:48 -0400

    Added initial files

---
+++ S7-COMPAT/seki_s7_compat_base/CfgWeapons.hpp
@@ -0,0 +1,65 @@
+class CfgWeapons {
+	class Missile_AGM_02_Plane_CAS_01_F;
+
+	class FIR_RKT_Base : Missile_AGM_02_Plane_CAS_01_F {};
+
+	class FIR_APKWS_Launcher: FIR_RKT_Base {
+        ace_hellfire_enabled = 1; // show attack profile / lock on hud
+        ace_laser_canSelect = 1; // can ace_laser lock (allows switching laser code)
+		ace_laser_showHud = 1;
+        canLock = 0;
+		weaponLockSystem = 0;
+	};
+
+    class rhs_weap_HellfireLauncher: Missile_AGM_02_Plane_CAS_01_F  {};
+	class RHS_weap_AGM114K_base_Launcher: rhs_weap_HellfireLauncher {}; 
+    class RHS_weap_AGM114K_Launcher: RHS_weap_AGM114K_base_Launcher{
+        weaponLockSystem = 0;
+        canLock = 0;
+        ace_hellfire_enabled = 1; // show attack profile / lock on hud
+        ace_laser_canSelect = 1; // can ace_laser lock (allows switching laser code)
+		ace_laser_showHud = 1;
+		modes[] = {"this"};
+		muzzles[] = {"this"};
+    };
+
+	class RHS_weap_AGM114N_base_Launcher: rhs_weap_HellfireLauncher {}; 
+    class RHS_weap_AGM114N_Launcher: RHS_weap_AGM114N_base_Launcher {
+        weaponLockSystem = 0;
+        canLock = 0;
+        ace_hellfire_enabled = 1; // show attack profile / lock on hud
+        ace_laser_canSelect = 1; // can ace_laser lock (allows switching laser code)
+		ace_laser_showHud = 1;
+		modes[] = {"this"};
+		muzzles[] = {"this"};
+    };
+
+	class RHS_weap_AGM114M_base_Launcher: rhs_weap_HellfireLauncher {}; 
+    class RHS_weap_AGM114M_Launcher: RHS_weap_AGM114M_base_Launcher {
+        weaponLockSystem = 0;
+        canLock = 0;
+        ace_hellfire_enabled = 1; // show attack profile / lock on hud
+        ace_laser_canSelect = 1; // can ace_laser lock (allows switching laser code)
+		ace_laser_showHud = 1;
+		modes[] = {"this"};
+		muzzles[] = {"this"};
+    };
+    class RHS_MELB_Hellfire: rhs_weap_HellfireLauncher {
+        weaponLockSystem = 0;
+        canLock = 0;
+        ace_hellfire_enabled = 1; // show attack profile / lock on hud
+        ace_laser_canSelect = 1; // can ace_laser lock (allows switching laser code)
+		ace_laser_showHud = 1;
+		modes[] = {"this"};
+		muzzles[] = {"this"};
+    };
+    class RHS_MELB_DAGR: rhs_weap_HellfireLauncher {
+        weaponLockSystem = 0;
+        canLock = 0;
+        ace_hellfire_enabled = 1; // show attack profile / lock on hud
+        ace_laser_canSelect = 1; // can ace_laser lock (allows switching laser code)
+		ace_laser_showHud = 1;
+		modes[] = {"this"};
+		muzzles[] = {"this"};
+    };
+};
---
+++ S7-COMPAT/seki_s7_compat_base/config.cpp
@@ -0,0 +1,30 @@
+class CfgPatches
+{
+	class S7_COMPAT
+	{
+		name="S7 COMPAT";
+		requiredVersion=1.98;
+		requiredAddons[]=
+		{
+			"FIR_AirWeaponSystem_US",
+			"FIR_PilotCrewPack_US",
+			"rhsusf_melb",
+			"uk3cb_baf_weapons_Ammo",
+			"uk3cb_baf_weapons_SmallArms",
+			"uk3cb_baf_weapons_Accessories",
+			"uk3cb_baf_weapons_Launchers",
+			"rhsusf_weapons",
+			"rhsusf_weapons2",
+			"rhsusf_weapons3",
+			"ace_missileguidance",
+			"ace_hellfire"
+		};
+		author="SekiGamer";
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+#include "CfgAmmo.hpp"
+#include "CfgMagazines.hpp"
+#include "CfgWeapons.hpp"
