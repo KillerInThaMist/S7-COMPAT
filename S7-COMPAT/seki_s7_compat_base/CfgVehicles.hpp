5c82c1ff7e2a9156a5a2782c0fae2f1378903587
Sean <instantiateseankim@gmail.com>
2021-10-07 23:21:41 -0400

    Fixed to use right class name in CfgVehicles

--- S7-COMPAT/seki_s7_compat_base/CfgVehicles.hpp
+++ S7-COMPAT/seki_s7_compat_base/CfgVehicles.hpp
@@ -1,12 +1,12 @@
 class CfgVehicles
 {
-	class Box_NATO_Base;
-	class Box_NATO_Ammo_F : Box_NATO_Base
+	class NATO_BOX_Base;
+	class Box_NATO_Ammo_F : NATO_BOX_Base
 	{
 		maximumLoad = 2500;
-	}
-	class Box_NATO_Grenades_F : Box_NATO_Base
+	};
+	class Box_NATO_Grenades_F : NATO_BOX_Base
 	{
 		maximumLoad = 1500;
-	}
+	};
 }
