3c198796e8b1cc963f20fddf1ca4e3216b0a9ff4
Sean <instantiateseankim@gmail.com>
2022-03-23 21:23:39 -0400

    Broken Code
    This shit don't work

--- S7-COMPAT/seki_s7_compat_base/CfgVehicles.hpp
+++ S7-COMPAT/seki_s7_compat_base/CfgVehicles.hpp
@@ -24,16 +24,46 @@ class CfgVehicles {
         };
     };*/
     //class Helicopter_Base_H;
-    class RHS_MELB_AH6M;
-    class RHS_MELB_AH6M_M: RHS_MELB_AH6M {
-        class Turrets {
-            class CopilotTurret {
-                class OpticsIn {
-                    class Wide {
-                        directionStabilized = 0;
-                    };
+    class RHS_MELB_base;
+    class RHS_MELB_base: RHS_MELB_base {
+        class Turrets;
+        class Turrets: Turrets{
+            class CopilotTurret;
+            class CopilotTurret: CopilotTurret {
+				class OpticsIn {
+					class Wide
+					{
+						opticsDisplayName="TRK COR";
+						initAngleX=0;
+						minAngleX=-360;
+						maxAngleX=360;
+						initAngleY=0;
+						minAngleY=-15;
+						maxAngleY=85;
+						initFov=0.30000001;
+						minFov=0.30000001;
+						maxFov=0.30000001;
+						visionMode[]=
+						{
+							"Normal",
+							"NVG",
+							"Ti"
+						};
+						thermalMode[]={0};
+						directionStabilized=1;
+						gunnerOpticsModel="rhsusf\addons\rhsusf_melb\data\optics\melb_flir_wf.p3d";
+						opticsPPEffects[]=
+						{
+							"OpticsCHAbera3",
+							"OpticsBlur3"
+						};
+						gunnerOpticsEffect[]=
+						{
+							"TankCommanderOptics2"
+						};
+					};
                 };
-            };
+			};
         };
     };
 }
