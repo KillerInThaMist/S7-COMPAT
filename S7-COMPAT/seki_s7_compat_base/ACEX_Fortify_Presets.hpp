28f15afcb1a844e87ae20dd9dd76f9d82bf8996d
Sean <instantiateseankim@gmail.com>
2022-10-14 00:13:01 -0400

    fixed more syntax errors and corrected object types

--- S7-COMPAT/seki_s7_compat_base/ACEX_Fortify_Presets.hpp
+++ S7-COMPAT/seki_s7_compat_base/ACEX_Fortify_Presets.hpp
@@ -1,16 +1,16 @@
 class ACEX_Fortify_Presets {
 	class s7standard {
-		displayName = "S7 Standard"
-		objects = {
+		displayName = "S7 Standard";
+		objects[] = {
 			{"Land_BagFence_Round_F", 5},
             {"Land_BagFence_Short_F", 5},
             {"Land_BagFence_Long_F", 10},
             {"Land_Plank_01_4m_F", 10},
 			{"Land_BagBunker_Small_F", 25},
-			{"Land_HBarrier_01_big_tower_green_F", 100},
-            {"Land_HBarrier_01_wall_4_green_F", 25},
-            {"Land_HBarrier_01_wall_corner_green_F", 25},
-            {"Land_HBarrier_01_line_1_green_F", 5}
+			{"Land_HBarrierTower_F", 100},
+            {"Land_HBarrierWall4_F", 25},
+            {"Land_HBarrierWall_corner_F", 25},
+            {"Land_HBarrier_1_F", 5}
 		};
 	};
-}
+};
