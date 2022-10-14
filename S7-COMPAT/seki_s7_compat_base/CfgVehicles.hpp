d6b8b59c41ac0f53808772c69e348301aeb082b0
Sean <instantiateseankim@gmail.com>
2022-10-14 00:29:47 -0400

    Added changes to crates for mechanized operations

--- S7-COMPAT/seki_s7_compat_base/CfgVehicles.hpp
+++ S7-COMPAT/seki_s7_compat_base/CfgVehicles.hpp
@@ -64,7 +64,7 @@ class CfgVehicles
 	class VirtualReammoBox_F;
 	class VirtualReammoBox_small_F : VirtualReammoBox_F
 	{
-		ace_rearm_defaultSupply = 4000;
+		ace_rearm_defaultSupply = 1200;
 		// Dragging
 		ace_dragging_canDrag = 1;  // Can be dragged (0-no, 1-yes)
         ace_dragging_dragPosition[] = {0, 2, 0};  // Offset of the model from the body while dragging (same as attachTo) (default: [0, 1.5, 0])
@@ -80,4 +80,16 @@ class CfgVehicles
 		ace_cargo_size = 2;
 		ace_cargo_noRename = 0;
 	};
+	class B_CargoNet_01_ammo_F {
+		// Dragging
+		ace_dragging_canDrag = 1;  // Can be dragged (0-no, 1-yes)
+        ace_dragging_dragPosition[] = {0, 2, 0};  // Offset of the model from the body while dragging (same as attachTo) (default: [0, 1.5, 0])
+        ace_dragging_dragDirection = 0;  // Model direction while dragging (same as setDir after attachTo) (default: 0)
+
+		// Carrying
+        ace_dragging_canCarry = 1;  // Can be carried (0-no, 1-yes)
+        ace_dragging_carryPosition[] = {0, 2, 0};  // Offset of the model from the body while dragging (same as attachTo) (default: [0, 1, 1])
+        ace_dragging_carryDirection = 0;  // Model direction while dragging (same as setDir after attachTo) (default: 0)
+
+	}
 }
