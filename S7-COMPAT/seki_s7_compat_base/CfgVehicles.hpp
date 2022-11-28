b0c35c406f5dbd27f1d4ea73d59de37321d307c7
Sean <instantiateseankim@gmail.com>
2022-11-27 23:38:06 -0500

    updated cargonet ace cargo space

--- S7-COMPAT/seki_s7_compat_base/CfgVehicles.hpp
+++ S7-COMPAT/seki_s7_compat_base/CfgVehicles.hpp
@@ -27,7 +27,7 @@ class CfgVehicles
 	class Land_FlexibleTank_01_forest_F : Land_FlexibleTank_01_F
 	{
 		ace_refuel_fuelCapacity = -10;
-		// Dragging
+		//Dragging
         ace_dragging_canDrag = 1;  // Can be dragged (0-no, 1-yes)
         ace_dragging_dragPosition[] = {0, 2, 0};  // Offset of the model from the body while dragging (same as attachTo) (default: [0, 1.5, 0])
         ace_dragging_dragDirection = 0;  // Model direction while dragging (same as setDir after attachTo) (default: 0)
@@ -93,6 +93,6 @@ class CfgVehicles
         ace_dragging_carryPosition[] = {0, 2, 0};  // Offset of the model from the body while dragging (same as attachTo) (default: [0, 1, 1])
         ace_dragging_carryDirection = 0;  // Model direction while dragging (same as setDir after attachTo) (default: 0)
 		
-		ace_cargo_space = 10;
+		ace_cargo_space = 6;
 	};
 };
