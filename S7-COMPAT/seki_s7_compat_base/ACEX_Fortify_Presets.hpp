32da855992f8708219a0fdecde1864f59ff763aa
KillerInThaMist <killerinthamist@gmail.com>
2024-01-30 02:29:50 -0500

    Update ACEX_Fortify_Presets.hpp
    Added
    +Lamp
    +Red Tarp

--- S7-COMPAT/seki_s7_compat_base/ACEX_Fortify_Presets.hpp
+++ S7-COMPAT/seki_s7_compat_base/ACEX_Fortify_Presets.hpp
@@ -2,15 +2,17 @@ class ACEX_Fortify_Presets {
 	class s7standard {
 		displayName = "S7 Standard";
 		objects[] = {
-			{"Land_BagFence_Round_F", 5},
-            {"Land_BagFence_Short_F", 5},
-            {"Land_BagFence_Long_F", 10},
-            {"Land_Plank_01_4m_F", 10},
+			{"Land_BagFence_Round_F", 3},
+            {"Land_BagFence_Short_F", 3},
+            {"Land_BagFence_Long_F", 5},
+            {"Land_Plank_01_4m_F", 3},
 			{"Land_BagBunker_Small_F", 25},
-			{"Land_HBarrierTower_F", 100},
-            {"Land_HBarrierWall4_F", 25},
-            {"Land_HBarrierWall_corner_F", 25},
-            {"Land_HBarrier_1_F", 5}
+			{"Land_HBarrierTower_F", 60},
+            {"Land_HBarrierWall4_F", 8},
+            {"Land_HBarrierWall_corner_F", 8},
+            {"Land_HBarrier_1_F", 5},
+			{"Land_PortableLight_double_F",5},
+			{"Tarp_01_Large_Red_F",5}
 		};
 	};
 };
